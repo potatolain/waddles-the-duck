af2fbbd4a1544e73ae49e1ca24c704d6d5612295
cppchriscpp <admin@cpprograms.net>
2016-10-30 11:33:15 -0400

    Minor level changes

--- lib/project_constants.asm
+++ lib/project_constants.asm
@@ -1,4 +1,4 @@
-.define  		BUILD 			2196  
+.define  		BUILD 			2201  
 .define 		VERSION 		"0.1a" 
-.define 		BUILD_DATE		"Sat 10/29/2016  0:08" 
-.define 		SPLASH_MESSAGE 	"Only 30 years late to the       party " 
+.define 		BUILD_DATE		"Sat 10/29/2016  1:54" 
+.define 		SPLASH_MESSAGE 	"Not a quack " 
