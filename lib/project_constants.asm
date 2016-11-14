a9e196b40fe85f42bed7431895f68c67cae0f6a5
cppchriscpp <admin@cpprograms.net>
2016-11-14 12:49:16 -0500

    Militarized turtle

--- lib/project_constants.asm
+++ lib/project_constants.asm
@@ -1,4 +1,4 @@
-.define  		BUILD 			2533  
+.define  		BUILD 			2537  
 .define 		VERSION 		"0.1a" 
-.define 		BUILD_DATE		"Mon 11/14/2016 12:19" 
-.define 		SPLASH_MESSAGE 	"Once starred a turtle " 
+.define 		BUILD_DATE		"Mon 11/14/2016 12:48" 
+.define 		SPLASH_MESSAGE 	"Only 30 years late to the       party " 
