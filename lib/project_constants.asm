bde9667adbeb4e32a5bcb8c25ce6cd71d3ad6dfb
cppchriscpp <admin@cpprograms.net>
2016-11-14 21:12:54 -0500

    You stepped on the turtle, you monster!

--- lib/project_constants.asm
+++ lib/project_constants.asm
@@ -1,4 +1,4 @@
-.define  		BUILD 			2537  
+.define  		BUILD 			2538  
 .define 		VERSION 		"0.1a" 
-.define 		BUILD_DATE		"Mon 11/14/2016 12:48" 
-.define 		SPLASH_MESSAGE 	"Only 30 years late to the       party " 
+.define 		BUILD_DATE		"Mon 11/14/2016 20:53" 
+.define 		SPLASH_MESSAGE 	"Not an RPG " 
