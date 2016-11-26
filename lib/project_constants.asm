d0d02b76e723b19b71aa0286f0fa7850b806c8f4
cppchriscpp <admin@cpprograms.net>
2016-11-26 13:58:01 -0500

    Change scrolling a bit to give you more warning of impending foes.

--- lib/project_constants.asm
+++ lib/project_constants.asm
@@ -1,4 +1,4 @@
-.define  		BUILD 			2929  
+.define  		BUILD 			2931  
 .define 		VERSION 		"0.1a" 
-.define 		BUILD_DATE		"Sat 11/26/2016  2:19" 
-.define 		SPLASH_MESSAGE 	"Honk! " 
+.define 		BUILD_DATE		"Sat 11/26/2016 13:55" 
+.define 		SPLASH_MESSAGE 	"Not a quack " 
