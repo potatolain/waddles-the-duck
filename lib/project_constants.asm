d48918a252c49428fb2d48eead3824288eaefce0
cppchriscpp <admin@cpprograms.net>
2016-12-05 19:35:52 -0500

    Tweak level 1 a bit

--- lib/project_constants.asm
+++ lib/project_constants.asm
@@ -1,4 +1,4 @@
-.define  		BUILD 			3451  
+.define  		BUILD 			3453  
 .define 		VERSION 		"0.1a" 
-.define 		BUILD_DATE		"Mon 12/05/2016 17:14" 
-.define 		SPLASH_MESSAGE 	"Don't scary " 
+.define 		BUILD_DATE		"Mon 12/05/2016 17:21" 
+.define 		SPLASH_MESSAGE 	"Smells oddly refreshing " 
