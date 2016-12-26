d5008f2bf5167cc5a99852f0b30f1c28e6295d98
cppchriscpp <admin@cpprograms.net>
2016-12-26 16:47:29 -0500

    Mess with level 3 design a little to make it more winnable

--- lib/project_constants.asm
+++ lib/project_constants.asm
@@ -1,4 +1,4 @@
-.define  		BUILD 			4193  
+.define  		BUILD 			4195  
 .define 		VERSION 		"0.1a" 
-.define 		BUILD_DATE		"Mon 12/26/2016 16:36" 
-.define 		SPLASH_MESSAGE 	"Don't scary " 
+.define 		BUILD_DATE		"Mon 12/26/2016 16:45" 
+.define 		SPLASH_MESSAGE 	"Somebody get this duck away     from me!!! " 
