fd26b3ddd35cd0d62db8fddf74ccd4c75ef05912
cppchriscpp <admin@cpprograms.net>
2016-12-28 20:17:08 -0500

    Try to improve duck animation

--- lib/project_constants.asm
+++ lib/project_constants.asm
@@ -1,4 +1,4 @@
-.define  		BUILD 			4502  
+.define  		BUILD 			4508  
 .define 		VERSION 		"0.1a" 
-.define 		BUILD_DATE		"Wed 12/28/2016 18:23" 
-.define 		SPLASH_MESSAGE 	"What is a duck anyway? " 
+.define 		BUILD_DATE		"Wed 12/28/2016 20:15" 
+.define 		SPLASH_MESSAGE 	"Do you even quack? " 
