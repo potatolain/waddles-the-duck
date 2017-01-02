946a90b80962f6bf09984ad53893cb4731c6f9c8
cppchriscpp <admin@cpprograms.net>
2017-01-02 16:56:17 -0500

    Fix a long-standing appearance bug stemming from the prior commit's bug.

--- lib/project_constants.asm
+++ lib/project_constants.asm
@@ -1,4 +1,4 @@
-.define  		BUILD 			4706  
+.define  		BUILD 			4722  
 .define 		VERSION 		"0.1a" 
-.define 		BUILD_DATE		"Mon 01/02/2017 16:51" 
-.define 		SPLASH_MESSAGE 	"Smells oddly refreshing " 
+.define 		BUILD_DATE		"Mon 01/02/2017 16:55" 
+.define 		SPLASH_MESSAGE 	"Will be at MAGFest 2017! " 
