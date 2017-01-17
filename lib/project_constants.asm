d2bd7357263fb876c1a3282267a6b8bd15cad0ad
cppchriscpp <admin@cpprograms.net>
2017-01-16 21:36:19 -0500

    Couple new tiles

--- lib/project_constants.asm
+++ lib/project_constants.asm
@@ -1,4 +1,4 @@
-.define  		BUILD 			4943  
+.define  		BUILD 			4950  
 .define 		VERSION 		"0.1a" 
-.define 		BUILD_DATE		"Sat 01/14/2017 23:36" 
-.define 		SPLASH_MESSAGE 	"My eyeballs are vibrating " 
+.define 		BUILD_DATE		"Sat 01/14/2017 23:52" 
+.define 		SPLASH_MESSAGE 	"Touch lava                      get crispy " 
