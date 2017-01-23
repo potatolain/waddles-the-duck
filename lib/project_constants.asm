c406a52a21d5ab53ea753dc59d555e5e336da2a3
cppchriscpp <admin@cpprograms.net>
2017-01-22 19:31:28 -0500

    Making the title/end more like our normal font

--- lib/project_constants.asm
+++ lib/project_constants.asm
@@ -1,4 +1,4 @@
-.define  		BUILD 			5710  
+.define  		BUILD 			5712  
 .define 		VERSION 		"0.1a" 
-.define 		BUILD_DATE		"Sun 01/22/2017 18:40" 
-.define 		SPLASH_MESSAGE 	"Pants not included. " 
+.define 		BUILD_DATE		"Sun 01/22/2017 19:25" 
+.define 		SPLASH_MESSAGE 	"Touch lava                      get crispy " 
