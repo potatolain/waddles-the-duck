abf65797379f8071927a19e5e7c864b47e8e2e21
cppchriscpp <admin@cpprograms.net>
2017-01-28 21:21:37 -0500

    Fixing typos, etc

--- lib/project_constants.asm
+++ lib/project_constants.asm
@@ -1,5 +1,5 @@
-.define  		BUILD 			5994  
+.define  		BUILD 			5998  
 .define 		VERSION 		"1.0-pre   " 
-.define 		BUILD_DATE		"Sat 01/28/2017 20:42" 
+.define 		BUILD_DATE		"Sat 01/28/2017 21:17" 
 .define 		SPLASH_MESSAGE 	"Prerelease Build" 
 .define 		COPYRIGHT		"Copyright 2016-2017 cpprograms" 
