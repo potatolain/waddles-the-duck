ff115a1c19c131b9bd0a7a9ddbb09132d96beab3
cppchriscpp <admin@cpprograms.net>
2017-02-05 15:20:54 -0500

    Some simple level design fixes

--- lib/project_constants.asm
+++ lib/project_constants.asm
@@ -1,7 +1,7 @@
-.define  		BUILD 				6115  
+.define  		BUILD 				6119  
 .define 		VERSION 			"1.0-pre   " 
-.define 		BUILD_DATE			"Mon 01/30/2017 21:16" 
+.define 		BUILD_DATE			"Sun 02/05/2017 15:19" 
 .define 		SPLASH_MESSAGE		"Prerelease Build" 
 .define 		COPYRIGHT			"Copyright 2016-2017 cpprograms" 
-.define 		COMMIT_COUNT		201 
+.define 		COMMIT_COUNT		202 
 .define 		CODE_LINE_COUNT		9429 
