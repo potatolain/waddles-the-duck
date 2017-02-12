5edd43bb8794d8c8aad53014d681aa632a9513a9
cppchriscpp <admin@cpprograms.net>
2017-02-11 19:12:56 -0500

    Few small level updates

--- lib/project_constants.asm
+++ lib/project_constants.asm
@@ -1,7 +1,7 @@
-.define  		BUILD 				6119  
+.define  		BUILD 				6122  
 .define 		VERSION 			"1.0-pre   " 
-.define 		BUILD_DATE			"Sun 02/05/2017 15:19" 
+.define 		BUILD_DATE			"Sat 02/11/2017 17:23" 
 .define 		SPLASH_MESSAGE		"Prerelease Build" 
 .define 		COPYRIGHT			"Copyright 2016-2017 cpprograms" 
-.define 		COMMIT_COUNT		202 
+.define 		COMMIT_COUNT		205 
 .define 		CODE_LINE_COUNT		9429 
