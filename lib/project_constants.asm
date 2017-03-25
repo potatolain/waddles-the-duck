d39b578212bc76c9cedaf35106b7bf84349fc36a
cppchriscpp <admin@cpprograms.net>
2017-03-24 22:06:53 -0400

    more level tweaks

--- lib/project_constants.asm
+++ lib/project_constants.asm
@@ -1,8 +1,8 @@
-.define  		BUILD 				6181  
+.define  		BUILD 				6190  
 .define 		VERSION 			"1.1       " 
-.define 		BUILD_DATE			"Fri 03/24/2017 21:00" 
+.define 		BUILD_DATE			"Fri 03/24/2017 22:05" 
 .define 		SPLASH_MESSAGE		"Post-Contest Build" 
 .define 		COPYRIGHT			"Copyright 2016-2017 cpprograms" 
-.define 		COMMIT_COUNT		217 
+.define 		COMMIT_COUNT		218 
 .define 		CODE_LINE_COUNT		9454 
-.define		SHOW_VERSION_STRING 1 
+.define		SHOW_VERSION_STRING 0 
--- makefile
+++ makefile
@@ -21,7 +21,7 @@ CONFIG_FILE=$(ROOT_DIR)/ca65-utils/nesgame-chr.cfg
 TEXT2DATA=sound/text2data
 NSF2DATA=sound/nsf2data
 VERSION=1.1       
-SHOW_VERSION_STRING=1
+SHOW_VERSION_STRING=0
 COPYRIGHT=Copyright 2016-2017 cpprograms
 
 ### USER EDITABLE STUFF ENDS HERE
